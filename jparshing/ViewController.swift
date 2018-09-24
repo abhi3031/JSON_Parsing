28338f7b1acf0e743a4b1bf392cd998745dacb2d
topspizza12 <42130369+topspizza12@users.noreply.github.com>
2018-09-24 14:08:40 +0530

    Initial commit

---
+++ jparshing/ViewController.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let obj = GetRailData().GujratQueen();
+        print(obj);
+        
+        
+   
+        
+    }
+
+    
+}
+
